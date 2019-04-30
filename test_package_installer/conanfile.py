c01103c9bdae2db472c835e18b6b8e6173bc777d
Uilian Ries <uilianries@gmail.com>
2019-04-30 20:33:28 +0700

    Update test_package_installer/conanfile.py
    Co-Authored-By: SSE4 <tomskside@gmail.com>

--- test_package_installer/conanfile.py
+++ test_package_installer/conanfile.py
@@ -7,5 +7,6 @@ from conans import ConanFile
 
 class TestPackageConan(ConanFile):
     def test(self):
-        self.run("flex --version", run_environment=True)
+        if not tools.cross_building(self.settings):
+            self.run("flex --version", run_environment=True)
         self.run("flex %s" % os.path.join(self.source_folder, "basic_nr.l"), run_environment=True)
