692d1102ad297d79079e6baf4a690592250fc372
SSE4 <tomskside@gmail.com>
2019-05-10 02:44:56 +0700

    - ensure MinGW settings are used
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -8,11 +8,22 @@ import os
 if __name__ == "__main__":
     if "CONAN_CONANFILE" in os.environ and os.environ["CONAN_CONANFILE"] == "conanfile_installer.py":
         arch = os.environ["ARCH"]
-        build_requires = dict()
-        if "MINGW_CONFIGURATIONS" in os.environ:
-            build_requires = {"*": ["mingw_installer/1.0@conan/stable"]}
         builder = build_template_installer.get_builder()
-        builder.add({"os": build_shared.get_os(), "arch_build": arch, "arch": arch}, {}, {}, build_requires)
+        settings = {"os": build_shared.get_os(), "arch_build": arch, "arch": arch}
+        if "MINGW_CONFIGURATIONS" in os.environ:
+            configs = os.environ["MINGW_CONFIGURATIONS"]
+            for config in configs.split(","):
+                tokens = config.strip().split('@')
+                settings["compiler"] = "gcc"
+                settings["compiler.version"] = tokens[0]
+                settings["arch"] = tokens[1]
+                settings["compiler.exception"] = tokens[2]
+                settings["compiler.threads"] = tokens[3]
+                settings["compiler.libcxx"] = "libstdc++11"
+                build_requires = {"*": ["mingw_installer/1.0@conan/stable"]}
+                builder.add(settings, {}, {}, build_requires)
+        else:
+            builder.add(settings, {}, {}, {})
         builder.run()
     else:
         builder = build_template_default.get_builder(pure_c=True)
