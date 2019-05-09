6ec2029399bec206e7a3410f0715ab1b2184d87b
SSE4 <tomskside@gmail.com>
2019-05-10 02:38:38 +0700

    - ensure mingw_installer is available
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -8,8 +8,11 @@ import os
 if __name__ == "__main__":
     if "CONAN_CONANFILE" in os.environ and os.environ["CONAN_CONANFILE"] == "conanfile_installer.py":
         arch = os.environ["ARCH"]
+        build_requires = dict()
+        if "MINGW_CONFIGURATIONS" in os.environ:
+            build_requires = {"*": ["mingw_installer/1.0@conan/stable"]}
         builder = build_template_installer.get_builder()
-        builder.add({"os": build_shared.get_os(), "arch_build": arch, "arch": arch}, {}, {}, {})
+        builder.add({"os": build_shared.get_os(), "arch_build": arch, "arch": arch}, {}, {}, build_requires)
         builder.run()
     else:
         builder = build_template_default.get_builder(pure_c=True)
