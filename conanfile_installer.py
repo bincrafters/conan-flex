0f3e4c4f372a3bfed221c1a77fb2bd2abaa022e6
SSE4 <tomskside@gmail.com>
2019-07-16 13:55:52 +0200

    - add include_build_settings
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- conanfile_installer.py
+++ conanfile_installer.py
@@ -13,6 +13,7 @@ class ConanFileInstaller(ConanFileBase):
     settings = "os_build", "arch_build", "compiler", "arch"
 
     def package_id(self):
+        self.info.include_build_settings()
         del self.info.settings.compiler
         del self.info.settings.arch
 
